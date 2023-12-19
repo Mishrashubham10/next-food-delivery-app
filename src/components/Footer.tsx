9ef15ea5ce634da09b864f2a9643391711addf69
Shubam Mishra <shubhammishra10101998@gmail.com>
2023-12-19 10:17:12 +0530

    creates footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='uppercase h-12 md:h-24 p-4 lg:20 xl:40 text-red-500 flex items-center justify-between'>
+      <Link href="/" className='uppercase fond-bold text-xl'>massimo</Link>
+      <p className='text-sm'>&Copy; all right reserved.</p> 
+    </footer>
   )
 }
 
