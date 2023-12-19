7e0b5a6835fcd6d5049689e8a2de92ad6f84e224
Shubam Mishra <shubhammishra10101998@gmail.com>
2023-12-19 16:38:49 +0530

    Some changes in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer className='uppercase h-12 md:h-24 p-4 lg:20 xl:40 text-red-500 flex items-center justify-between'>
+    <footer className='uppercase h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between'>
       <Link href="/" className='uppercase fond-bold text-xl'>massimo</Link>
       <p className='text-sm'>&Copy; all right reserved.</p> 
     </footer>
